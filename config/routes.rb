7c6c83ba481b39bb24030282f3cbfaa7b3fb0277
rodrigonm <rdrg.n.magalhaes@gmail.com>
2019-06-30 00:21:16 -0400

    Added nested route to board and task

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
-  resources :tasks do
-    get :check
-    get :uncheck
-  end
   root 'boards#index'
-  resources :boards
+  resources :boards do
+    resources :tasks do
+      get :check
+      get :uncheck
+    end
+  end
+  resources :tasks
+  resources :stages
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
