a894ac49c79119a34f0c12787031628063d3223c
rodrigonm <rdrg.n.magalhaes@gmail.com>
2019-07-01 22:23:26 -0400

    added validation on tasks model attributes

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,10 @@
 class Task < ApplicationRecord
+
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :board_id, presence: true
+  validates :stage_id, presence: true
+
   belongs_to :board
   belongs_to :stage
 end
